ad257051b363375dfc81af66d5d4a54f4d0e9481
marquis-sharefbs <official.sharefmbs@gmail.com>
2026-05-01 11:25:28 -0700

    Add Python script project to run local server on host machine to demo and simulate changes to website. Updated index.html with changes to commented code, project section, and about section.

---
+++ host-machine-server.sh
@@ -0,0 +1,8 @@
+# Direct to folder containing web files
+cd ~/Documents/coding/projects/personal-portfolio-website/
+
+# Use Python to create a local server on host machine
+python3 -m http.server 8000
+
+# Open run the server in host machine's browser
+http://localhost:8000
